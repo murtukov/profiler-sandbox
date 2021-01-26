d02588649c0ecf8ba2f4af8c05ea43d4fd57a6e8
murtukov <murtukov@gmail.com>
2021-01-26 14:13:32 +0100

    Interim

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,10 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Overblog\GraphQL\ProfilerBundle\GraphQLProfilerBundle::class => ['all' => true],
 ];
